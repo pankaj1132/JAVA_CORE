4018142d519ecc266ee4cb92d978b011b207e6b0
pankaj1132 <pankaj17469@gmail.com>
2024-10-23 23:16:45 +0530

    add

---
+++ Object/student.java
@@ -0,0 +1,26 @@
+package ObjectClass.HashCode;
+
+public class student {
+    int sid;
+    String sname;
+
+    public student(int sid, String sname) {
+        this.sid = sid;
+        this.sname = sname;
+    }
+    public boolean  equals(Object o){
+        student s3=(student)o;
+        return this.sname==s3.sname && this.sid == s3.sid;
+    }
+    public int hashCode(){
+        return Integer.hashCode(sid)+sname.hashCode();
+    }
+    public static void main(String[] args) {
+        student s1=new student(123,"ram");
+        student s2=new student(123,"ram");
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        
+    }
+}
